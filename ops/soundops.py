49dc7da97b78e79199b87d7ab0b48b804a1f3259
Thomas Li <li.thomas3124@gmail.com>
2019-06-07 15:16:45 -0700

    To numpy array

--- ops/soundops.py
+++ ops/soundops.py
@@ -1,18 +1,23 @@
+import pydub 
 import numpy as np
-import subprocess as sp
 
-def audio_to_np(filename)
-    FFMPEG_BIN = "ffmpeg.exe"
+def read(f, normalized=False):
+    """MP3 to numpy array"""
+    a = pydub.AudioSegment.from_mp3(f)
+    y = np.array(a.get_array_of_samples())
+    if a.channels == 2:
+        y = y.reshape((-1, 2))
+    if normalized:
+        return a.frame_rate, np.float32(y) / 2**15
+    else:
+        return a.frame_rate, y
 
-    command = [FFMPEG_BIN,
-               '-i', filename,
-               '-f', 's16le',
-               '-acodec', 'pcm_s16le',
-               '-ar', '44100',  # ouput will have 44100 Hz
-               '-ac', '2',  # stereo (set to '1' for mono)
-               '-']
-    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
-    raw_audio = pipe.proc.stdout.read(88200 * 4)
-    audio_array = np.fromstring(raw_audio, dtype="int16")
-    audio_array = audio_array.reshape((len(audio_array) / 2, 2))
-    return audio_array
+def write(f, sr, x, normalized=False):
+    """numpy array to MP3"""
+    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
+    if normalized:  # normalized array - each item should be a float in [-1, 1)
+        y = np.int16(x * 2 ** 15)
+    else:
+        y = np.int16(x)
+    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
+    song.export(f, format="mp3", bitrate="320k")
