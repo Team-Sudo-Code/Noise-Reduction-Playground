38b803b18017204a658ac3befccc870da16de405
azhang1001 <49007939+azhang1001@users.noreply.github.com>
2019-06-07 16:59:30 -0700

    updated

--- ops/trim.py
+++ ops/trim.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 import glob
-files = glob.glob('filepath')
+files = glob.glob("/Users/alexa/OneDrive/Documents/GitHub/Audio-Segmentation/ops/*.wav*")
 for file in files:
     newAudio = AudioSegment.from_wav(file)[0:10000]  # milliseconds
-    newAudio.export('./new/'+file.getName(), format="wav")  # exports to a .wav file to a different path
+    newAudio.export(file.rsplit('\\',1)[0]+"/new/"+file.rsplit('\\',1)[1], format="wav")  # exports to a .wav file to a different path
 
