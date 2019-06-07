1e199d5010853756ba443240eb81beb16f2660a9
Thomas Li <li.thomas3124@gmail.com>
2019-06-07 14:49:17 -0700

    Cleanup

--- ops/soundops.py
+++ ops/soundops.py
@@ -1,14 +1,18 @@
 import numpy as np
-import subprocess
+import subprocess as sp
 
-FFMPEG_BIN = "ffmpeg.exe"
+def audio_to_np(filename)
+    FFMPEG_BIN = "ffmpeg.exe"
 
-command = [FFMPEG_BIN,
-           '-i', 'mySong.mp3',
-           '-f', 's16le',
-           '-acodec', 'pcm_s16le',
-           '-ar', '44100',  # ouput will have 44100 Hz
-           '-ac', '2',  # stereo (set to '1' for mono)
-           '-']
-output=subprocess.run(command, capture_output=True)
-
+    command = [FFMPEG_BIN,
+               '-i', filename,
+               '-f', 's16le',
+               '-acodec', 'pcm_s16le',
+               '-ar', '44100',  # ouput will have 44100 Hz
+               '-ac', '2',  # stereo (set to '1' for mono)
+               '-']
+    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
+    raw_audio = pipe.proc.stdout.read(88200 * 4)
+    audio_array = np.fromstring(raw_audio, dtype="int16")
+    audio_array = audio_array.reshape((len(audio_array) / 2, 2))
+    return audio_array
---
+++ ops/urltosound.py
@@ -0,0 +1,1 @@
+#Zander's job
