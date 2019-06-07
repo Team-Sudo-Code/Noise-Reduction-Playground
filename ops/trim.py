6bf9d1b034965d5cfbf5314ed968ad5c4de08d80
azhang1001 <49007939+azhang1001@users.noreply.github.com>
2019-06-07 15:49:54 -0700

    Update trim.py

--- ops/trim.py
+++ ops/trim.py
@@ -1,1 +1,8 @@
-import pydub
+from pydub import AudioSegment
+import glob
+path = 'fill this in'
+files = glob.glob(path)
+for file in files:
+    newAudio = AudioSegment.from_wav(file)
+    newAudio = newAudio[0:10000] # milliseconds
+    newAudio.export('./new/'+file.getName(), format="wav") # Exports to a wav file to a different path
