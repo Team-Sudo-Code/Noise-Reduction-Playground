b76ed7487cf2b252a8fa56c105e9a9b2e0d122d9
Rahul Kaura <rahulkaura2004@gmail.com>
2019-06-07 16:33:13 -0700

    Update trim.py

--- ops/trim.py
+++ ops/trim.py
@@ -4,3 +4,5 @@ files = glob.glob('')
 for file in files:
     newAudio = AudioSegment.from_wav(file)[0:10000] # milliseconds
     newAudio.export('./new/'+file.getName(), format="wav") # Exports to a wav file to a different path
+#Code for the project for trim.py which is trimming the wav files that Kento will upload
+#Need to run the code that Alex has created
