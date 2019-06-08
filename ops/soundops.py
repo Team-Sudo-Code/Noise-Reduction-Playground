cca1e2b5b4ea03d768a1af530bf9e19fb7242d4b
Thomas Li <li.thomas3124@gmail.com>
2019-06-07 20:02:40 -0700

    Reviese soundops

--- ops/soundops.py
+++ ops/soundops.py
@@ -1,23 +1,8 @@
 import pydub 
 import numpy as np
+from scipy.io.wavfile import read
 
-def read(f, normalized=False):
-    """MP3 to numpy array"""
-    a = pydub.AudioSegment.from_mp3(f)
-    y = np.array(a.get_array_of_samples())
-    if a.channels == 2:
-        y = y.reshape((-1, 2))
-    if normalized:
-        return a.frame_rate, np.float32(y) / 2**15
-    else:
-        return a.frame_rate, y
-
-def write(f, sr, x, normalized=False):
-    """numpy array to MP3"""
-    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
-    if normalized:  # normalized array - each item should be a float in [-1, 1)
-        y = np.int16(x * 2 ** 15)
-    else:
-        y = np.int16(x)
-    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
-    song.export(f, format="mp3", bitrate="320k")
+def read(filepath):
+    """Reads a .wav file to a numpy array"""
+    data=read(filepath)
+    return np.array(data[1], dtype=float)
