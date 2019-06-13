3cccd58e56d6c081e12c4dc0495d7798f4235fda
Thomas Li <thomasli1234567890@gmail.com>
2019-06-13 16:02:26 -0700

    Housekeeping

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,3 @@
 @echo off
-Rem This deploys to Kento's Jetson Nano, you need the kento_id private key pair
-ssh kento@10.0.0.112
-cd Documents/audiocleanup
-python train.py
+Rem This deploys to Kento's Jetson Nano
+curl -d '{"build":1}' -H "Content-Type: application/json" -X POST http://teamsudocode.herokuapp.com/post
