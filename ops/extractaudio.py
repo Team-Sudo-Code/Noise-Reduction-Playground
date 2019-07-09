e0e594bcbe2b3be1c06d9a63649b8c65c4555f8b
Thomas Li <thomasli1234567890@gmail.com>
2019-07-09 15:23:58 -0700

    Cleanup after rahul

--- ops/extractaudio.py
+++ ops/extractaudio.py
@@ -1,17 +1,20 @@
-import os
+import glob
+
 
 def main():
-    files = os.listdir("./")
+    files = glob.glob("./*.mp4")
+    print("Discovered {} files".format(len(files)))
     for f in files:
         if f.lower()[-3:] == "mp4":
-            print
-            "processing", f
+            print("processing {}".format(f))
             process(f)
 
+
 def process(f):
-    inFile = f
-    outFile = f[:-3] + "mp3"
-    cmd = "ffmpeg -i {} -vn  -ac 2 -ar 44100 -ab 320k -f mp3 {}".format(inFile, outFile)
+    fin = f
+    fout = f[:-3] + "mp3"
+    cmd = "ffmpeg -i {} -vn  -ac 2 -ar 44100 -ab 320k -f wav {}".format(fin, fout)
     os.popen(cmd)
+
 
 main()
