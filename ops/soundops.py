6bc5cb19e053ca3bb14c466579c995c98a6990a2
Thomas Li <thomasli1234567890@gmail.com>
2019-08-09 15:20:18 -0700

    Added fast fourier transform(kindof)

--- ops/soundops.py
+++ ops/soundops.py
@@ -1,8 +1,17 @@
-import pydub 
-import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io.wavfile import read
+import numpy as np
 
-def read(filepath):
-    """Reads a .wav file to a numpy array"""
-    data=read(filepath)
-    return np.array(data[1], dtype=float)
+def frequency_extraction(audio, samplingrate):
+    transform = np.fft.fft(audio)
+    freqs = np.fft.fftfreq(len(transform))
+    idx = np.argmax(np.abs(transform))
+    freq = freqs[idx]*samplingrate
+    return freq
+    
+def show_specgram(audiodata, samplingrate):
+    plt.specgram(audiodata, samplingrate)
+
+
+samplingrate, audiodata = read("basictone.wav")
+freq=frequency_extraction(audiodata, samplingrate)
